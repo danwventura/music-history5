fd34016df94502ddc926f6cd3d2ddd5bafc90a96
Dan Ventura <danwventura@icloud.com>
2016-05-02 17:47:16 -0500

    got more button at the bottom/deleting only one song in second set

--- mh5.js
+++ mh5.js
@@ -4,12 +4,6 @@ $(document).ready(function(){
 
 // jQuery
 let listView = $('#list-view');
-
-//jQuery
-let moreButton = $('#more')
-// let moreButton = $("<input type="button" value="More"></input>")
-
-
 let songs = [];
 let songs2 = [];
 
@@ -38,23 +32,21 @@ function printSongs(songs){
 
 
     listView.append(bigString)
-    // listView.append(moreButton)
-    deleteSongs(i)
+    deleteSongs()
   }
 };
 
 
-function deleteSongs(i){
+function deleteSongs(){
   let deleteBtn = $('.delete');
-  deleteBtn.click(function(){
-    event.target.parentElement.remove()
+  deleteBtn.click(function(e){
+    e.target.closest('div').remove()
   })
 }
 
-loadSongs();
 
 
-moreButton.click(function loadSongs2 () {
+$('#moreButton').click(function loadSongs2 () {
   let loader = new XMLHttpRequest();
 
   loader.addEventListener("load", function () {
@@ -67,22 +59,24 @@ moreButton.click(function loadSongs2 () {
   })
 
 function printSongs2 (songs2){
-
+  
+  let secondOutput = $('#secondOutput')
   for (var i = 0; i < songs2.length; i++) {
     let currSong2 = songs2[i]
     let bigString2 = `<div><p>${currSong2.name} by ${currSong2.artist} on the album ${currSong2.album} <input type="button" class="delete2" value="Delete"></input></div>`
-
-    listView.append(bigString2)
-    // listView.append(moreButton)
-    deleteSongs2(i)
+console.log("this");
+    secondOutput.append(bigString2)
+    deleteSongs2()
   }
 }
 
-function deleteSongs2(i){
+function deleteSongs2(){
+  console.log("that");
   let deleteBtn2 = $('.delete2');
-  deleteBtn2.click(function(){
-    event.target.parentElement.remove()
+  deleteBtn2.click(function(e){
+    e.target.closest('div').remove()
   })
 }
 
+loadSongs();
 });
